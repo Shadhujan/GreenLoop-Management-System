4307d1c684f0a7ef533330b02f497b6b497af7bd
shadhujan <jeya38shadhujan@gmail.com>
2026-05-31 06:08:37 +0530

    feat(main): Change the boilerplate main.java file to working file

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import ui.DashboardFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new DashboardFrame();
+        });
+    }
+}
